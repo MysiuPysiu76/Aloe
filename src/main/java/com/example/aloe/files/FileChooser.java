d42c9a99ce9d7db7152387747b519ce7c6933e27
Michał Hrycek <kotyk9921@gmail.com>
2025-04-29 14:03:13 +0200

    Created documentation for FileChooser, FilesOpener class
    Created documentation for FileChooser, FilesOpener class

--- src/main/java/com/example/aloe/files/FileChooser.java
+++ src/main/java/com/example/aloe/files/FileChooser.java
@@ -6,8 +6,25 @@ import javafx.stage.DirectoryChooser;
 
 import java.io.File;
 
+/**
+ * The {@code FileChooser} class provides utility methods for opening
+ * file and directory chooser dialogs in the application.
+ * <p>
+ * It uses JavaFX {@link javafx.stage.FileChooser} and {@link javafx.stage.DirectoryChooser}
+ * to let users select files or folders from the file system.
+ * The initial directory is set to the current working directory tracked by {@link CurrentDirectory}.
+ * <p>
+ * Dialog titles are translated using the {@link Translator} class to support multiple languages.
+ *
+ * @since 1.7.7
+ */
 public class FileChooser {
 
+    /**
+     * Opens a directory chooser dialog allowing the user to select a folder.
+     *
+     * @return the selected directory as a {@link File}, or {@code null} if the user cancels the operation
+     */
     public static File chooseDirectory() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         directoryChooser.setTitle(Translator.translate("window.other.chose-directory"));
@@ -15,6 +32,13 @@ public class FileChooser {
         return directoryChooser.showDialog(Main.scene.getWindow());
     }
 
+    /**
+     * Opens a file chooser dialog allowing the user to select a file.
+     * <p>
+     * Only one file can be selected at a time. The dialog allows all file types.
+     *
+     * @return the selected file as a {@link File}, or {@code null} if the user cancels the operation
+     */
     public static File chooseFile() {
         javafx.stage.FileChooser fileChooser = new javafx.stage.FileChooser();
         fileChooser.setTitle(Translator.translate("window.other.choose-file"));
--- src/main/java/com/example/aloe/files/FilesOpener.java
+++ src/main/java/com/example/aloe/files/FilesOpener.java
@@ -5,8 +5,38 @@ import com.example.aloe.files.tasks.FileOpenerTask;
 
 import java.io.File;
 
+/**
+ * The {@code FilesOpener} class is responsible for delegating the logic
+ * of opening files and directories.
+ * <p>
+ * This utility determines whether the provided {@link File} is a regular file
+ * or a directory, and delegates the handling appropriately:
+ * <ul>
+ *   <li>If the file is a regular file, it creates and runs a {@link FileOpenerTask}.</li>
+ *   <li>If the file is a directory, it passes it to the {@link FilesLoader} to load its contents.</li>
+ * </ul>
+ *
+ * <p>Usage example:
+ * <pre>{@code
+ * File file = new File("path/to/file/or/directory");
+ * FilesOpener.open(file);
+ * }</pre>
+ *
+ * This class is typically used in response to user interactions, such as clicks in a file browser UI.
+ *
+ * @since 1.7.7
+ */
 public class FilesOpener {
 
+    /**
+     * Opens the given file or directory by delegating to the appropriate handler.
+     *
+     * @param file the file or directory to open
+     *  <ul>
+     *      <li>If it is a file, a {@link FileOpenerTask} is executed.</li>
+     *      <li>If it is a directory, {@link FilesLoader#load(File)} is called to load its contents.</li>
+     *  </ul>
+     */
     public static void open(File file) {
         if (file.isFile()) {
             new FileOpenerTask(file, true);
