0d396dffba7f36a3d3b9426fa4a1d2acd5decf45
Michał Hrycek <kotyk9921@gmail.com>
2025-04-30 15:54:11 +0200

    Created documentation for ACLPermissions class
    Created documentation for ACLPermissions class

--- src/main/java/com/example/aloe/files/permissions/ACLPermissions.java
+++ src/main/java/com/example/aloe/files/permissions/ACLPermissions.java
@@ -10,28 +10,85 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+/**
+ * A class for managing file access control lists (ACLs) on systems that support the {@link AclFileAttributeView}.
+ * <p>
+ * This implementation allows reading and writing detailed permissions for a specific user using ACL entries.
+ * The permissions are mapped to {@link AclEntryPermission} values and handled as a list of booleans.
+ * </p>
+ *
+ * <p>
+ * Usage:
+ * <ul>
+ *   <li>Set the target file via constructor</li>
+ *   <li>Set the target user via {@link #setUserName(String)}</li>
+ *   <li>Use {@link #loadPermissions()} and {@link #savePermissions(List)} for permission management</li>
+ * </ul>
+ * </p>
+ *
+ * The permissions list includes:
+ * <pre>
+ * READ_DATA, WRITE_DATA, APPEND_DATA, READ_NAMED_ATTRS, WRITE_NAMED_ATTRS,
+ * EXECUTE, DELETE_CHILD, READ_ATTRIBUTES, WRITE_ATTRIBUTES,
+ * DELETE, READ_ACL, WRITE_ACL, WRITE_OWNER, SYNCHRONIZE
+ * </pre>
+ *
+ * @see Permissions
+ * @see POSIXPermissions
+ * @see AclFileAttributeView
+ * @see AclEntryPermission
+ *
+ * @since 1.8.0
+ */
 public class ACLPermissions implements Permissions {
 
     final private File file;
     private String userName;
 
+    /**
+     * Constructs an {@code ACLPermissions} instance for the given file or directory.
+     *
+     * @param file the target file or directory
+     */
     public ACLPermissions(File file) {
         this.file = file;
     }
 
+    /**
+     * Gets the name of the user whose ACL permissions are managed.
+     *
+     * @return the username
+     */
     public String getUserName() {
         return this.userName;
     }
 
+    /**
+     * Sets the name of the user whose ACL permissions should be managed.
+     *
+     * @param userName the target username
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Returns a list of supported ACL permission names.
+     *
+     * @return a list of permission names in {@link AclEntryPermission} order
+     */
     @Override
     public List<String> getPermissionsList() {
         return List.of("READ_DATA", "WRITE_DATA", "APPEND_DATA", "READ_NAMED_ATTRS", "WRITE_NAMED_ATTRS", "EXECUTE", "DELETE_CHILD", "READ_ATTRIBUTES", "WRITE_ATTRIBUTES", "DELETE", "READ_ACL", "WRITE_ACL", "WRITE_OWNER", "SYNCHRONIZE");
     }
 
+    /**
+     * Loads the ACL permissions for the configured user on the associated file.
+     * The permissions are returned as a list of booleans corresponding to {@link #getPermissionsList()}.
+     *
+     * @return a list of booleans indicating granted permissions
+     * @throws NullPointerException if the username is not set
+     */
     @Override
     public List<Boolean> loadPermissions() {
         if (this.userName == null) throw new NullPointerException("User name must be set");
@@ -61,6 +118,13 @@ public class ACLPermissions implements Permissions {
         return permissions;
     }
 
+    /**
+     * Saves the specified permissions to the ACL for the configured user on the associated file.
+     * Any existing ACL entries for that user are removed and replaced.
+     *
+     * @param permissionsList a list of booleans indicating which permissions should be granted
+     * @throws NullPointerException if the username is not set
+     */
     @Override
     public void savePermissions(List<Boolean> permissionsList) {
         if (this.userName == null) throw new NullPointerException("User name must be set");
