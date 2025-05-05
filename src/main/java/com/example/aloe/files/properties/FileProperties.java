ddbc22cc03e080493d60f1c8bd44391b86b0c592
Michał Hrycek <kotyk9921@gmail.com>
2025-05-05 18:41:31 +0200

    Created documentation for FileProperties class and Properties interface
    Created documentation for FileProperties class and Properties interface

--- src/main/java/com/example/aloe/files/properties/FileProperties.java
+++ src/main/java/com/example/aloe/files/properties/FileProperties.java
@@ -19,16 +19,42 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-public record FileProperties(@NotNull File file) implements Properties{
+/**
+ * A class that provides various properties of a file or directory, such as name, size, type,
+ * creation and modification time, and more.
+ * <p>
+ * This class implements the {@link Properties} interface and provides localized descriptions
+ * using a {@link Translator}. It can be used to extract and display metadata about a file or
+ * directory in a user-friendly format.
+ *
+ * @since 1.9.4
+ */
+public record FileProperties(@NotNull File file) implements Properties {
 
+    /**
+     * Returns the name of the file or directory.
+     *
+     * @return the file name
+     */
     public String getName() {
         return this.file.getName();
     }
 
+    /**
+     * Returns the absolute path of the file or directory.
+     *
+     * @return the file path
+     */
     public String getPath() {
         return this.file.getPath();
     }
 
+    /**
+     * Returns the MIME type of the file. If the file is a directory,
+     * a localized string representing a folder is returned.
+     *
+     * @return the MIME type or folder label
+     */
     public String getType() {
         if (this.file.isFile()) {
             try {
@@ -41,6 +67,12 @@ public record FileProperties(@NotNull File file) implements Properties{
         }
     }
 
+    /**
+     * Returns the size of the file or the total size of contents in the directory,
+     * formatted with units and in bytes.
+     *
+     * @return formatted file or directory size
+     */
     public String getSize() {
         if (this.file.isDirectory()) {
             long directorySize = FilesUtils.calculateFileSize(this.file);
@@ -50,6 +82,11 @@ public record FileProperties(@NotNull File file) implements Properties{
         }
     }
 
+    /**
+     * Returns a short, human-readable size string or item count for directories.
+     *
+     * @return short size or item count string
+     */
     public String getShortSize() {
         if (this.file.isFile()) {
             return UnitConverter.convert(this.file.length());
@@ -58,10 +95,20 @@ public record FileProperties(@NotNull File file) implements Properties{
         }
     }
 
+    /**
+     * Returns the parent directory path of the file or directory.
+     *
+     * @return the parent path
+     */
     public String getParent() {
         return this.file.getParent();
     }
 
+    /**
+     * Returns the number of items in the directory.
+     *
+     * @return item count as string
+     */
     public String getItemsCount() {
         try {
             return String.valueOf(Files.list(Path.of(this.file.getPath())).count());
@@ -70,6 +117,11 @@ public record FileProperties(@NotNull File file) implements Properties{
         }
     }
 
+    /**
+     * Returns the creation time of the file or directory formatted as "dd-MM-yyyy HH:mm:ss".
+     *
+     * @return formatted creation time
+     */
     public String getCreationTime() {
         try {
             FileTime creationTime = Files.readAttributes(this.file.toPath(), BasicFileAttributes.class).creationTime();
@@ -79,6 +131,11 @@ public record FileProperties(@NotNull File file) implements Properties{
         }
     }
 
+    /**
+     * Returns the last modification time of the file or directory formatted as "dd-MM-yyyy HH:mm:ss".
+     *
+     * @return formatted modification time
+     */
     public String getModifiedTime() {
         LocalDateTime modifiedDate = LocalDateTime.ofInstant(
                 Instant.ofEpochMilli(this.file.lastModified()),
@@ -87,10 +144,20 @@ public record FileProperties(@NotNull File file) implements Properties{
         return modifiedDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss"));
     }
 
+    /**
+     * Returns the amount of free disk space available on the partition where the file resides.
+     *
+     * @return formatted free space
+     */
     public String getFreeSpace() {
         return UnitConverter.convert(this.file.getFreeSpace());
     }
 
+    /**
+     * Returns a list of localized property names to be displayed in the UI.
+     *
+     * @return list of property names
+     */
     @Override
     public List<String> getPropertiesNames() {
         List<String> names = new ArrayList<>();
@@ -108,6 +175,11 @@ public record FileProperties(@NotNull File file) implements Properties{
         return names;
     }
 
+    /**
+     * Returns a list of property values corresponding to the names returned by {@link #getPropertiesNames()}.
+     *
+     * @return list of property values
+     */
     @Override
     public List<String> getPropertiesValues() {
         List<String> values = new ArrayList<>();
@@ -125,6 +197,12 @@ public record FileProperties(@NotNull File file) implements Properties{
         return values;
     }
 
+    /**
+     * Returns a map of property names and corresponding values.
+     * Useful for displaying or exporting file metadata.
+     *
+     * @return map of file properties
+     */
     @Override
     public Map<String, String> getProperties() {
         List<String> names = getPropertiesNames();
--- src/main/java/com/example/aloe/files/properties/Properties.java
+++ src/main/java/com/example/aloe/files/properties/Properties.java
@@ -3,11 +3,50 @@ package com.example.aloe.files.properties;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a generic interface for accessing file or directory properties.
+ * <p>
+ * Implementations of this interface should provide a set of human-readable property names
+ * and their corresponding values, which can be displayed in user interfaces or exported.
+ * </p>
+ *
+ * Typical properties may include file name, size, type, creation time, modification time,
+ * parent directory, and so on.
+ *
+ * @since 1.9.4
+ */
 public interface Properties {
 
-    public Map<String, String> getProperties();
+    /**
+     * Returns a map of property names and their corresponding values.
+     * <p>
+     * The keys in the map represent localized or descriptive property names,
+     * while the values represent the associated property values.
+     * </p>
+     *
+     * @return a map of property name-value pairs
+     */
+    Map<String, String> getProperties();
 
-    public List<String> getPropertiesNames();
+    /**
+     * Returns a list of property names.
+     * <p>
+     * These names are typically localized strings used for displaying
+     * property labels in a user interface.
+     * </p>
+     *
+     * @return a list of property names
+     */
+    List<String> getPropertiesNames();
 
-    public List<String> getPropertiesValues();
+    /**
+     * Returns a list of property values.
+     * <p>
+     * Each value corresponds to the property name at the same index
+     * in the list returned by {@link #getPropertiesNames()}.
+     * </p>
+     *
+     * @return a list of property values
+     */
+    List<String> getPropertiesValues();
 }
