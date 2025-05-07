f75b0ce0a107c3c71ec1fdb60f6bde4fa5e25e4f
Michał Hrycek <kotyk9921@gmail.com>
2025-05-07 15:21:01 +0200

    Added missing translations to polish
    Added missing translations to polish and sorted translation for english

--- src/main/java/com/example/aloe/window/AboutWindow.java
+++ src/main/java/com/example/aloe/window/AboutWindow.java
@@ -76,7 +76,7 @@ public class AboutWindow extends Stage {
 
         Label name = new Label("Aloe");
         name.getStyleClass().addAll("name", "text");
-        Label version = new Label("2.0.6");
+        Label version = new Label("2.0.7");
         version.getStyleClass().addAll("version", "text");
         Label description = new Label(Translator.translate("window.about.description"));
         description.getStyleClass().addAll("description", "text");
