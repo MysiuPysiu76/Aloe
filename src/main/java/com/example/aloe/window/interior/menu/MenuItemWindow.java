a3f2a7da2cdac25e3a1720f815aac7f1d180ccf1
Michał Hrycek <kotyk9921@gmail.com>
2025-05-09 16:20:29 +0200

    Created documentation for MenuItemWindow class
    Created documentation for MenuItemWindow class

--- src/main/java/com/example/aloe/window/interior/menu/MenuItemWindow.java
+++ src/main/java/com/example/aloe/window/interior/menu/MenuItemWindow.java
@@ -15,33 +15,71 @@ import javafx.util.Callback;
 import org.kordamp.ikonli.fontawesome.FontAwesome;
 import org.kordamp.ikonli.javafx.FontIcon;
 
+/**
+ * {@code MenuItemWindow} is a specialized internal window used for creating or editing
+ * custom menu entries. It allows the user to specify a title, a file or URL path,
+ * and select an icon from the {@link FontAwesome} set.
+ *
+ * <p>This class extends {@link InteriorWindow} and assembles a structured form layout
+ * including input fields and a styled combo box with icon previews.</p>
+ *
+ * <p>The icon selection list is visually enhanced with {@link FontIcon} components to
+ * preview each icon next to its identifier. All labels and prompts are localized using
+ * {@link Translator}.</p>
+ *
+ * @see InteriorWindow
+ */
 class MenuItemWindow extends InteriorWindow {
 
+    /** Text field for the menu item title */
     protected TextField title;
+
+    /** Text field for the path (e.g., file path, URL) */
     protected TextField path;
+
+    /** ComboBox for selecting a FontAwesome icon */
     protected ComboBox<FontAwesome> icon;
 
+    /**
+     * Constructs a {@code MenuItemWindow} allowing the user to edit or define a menu entry.
+     *
+     * @param name the window title (localized)
+     * @param titleText the initial text for the title input field
+     * @param pathText the initial text for the path input field
+     * @param iconText the identifier of the initially selected FontAwesome icon
+     */
     public MenuItemWindow(String name, String titleText, String pathText, String iconText) {
         confirmButton = getConfirmButton(Translator.translate("window.interior.menu.save"));
 
         title = getInput(titleText, Translator.translate("window.interior.menu.title"));
         title.setStyle(String.format("-fx-border-color: %s;", Settings.getColor()));
+
         path = getInput(pathText, Translator.translate("window.interior.menu.example-path"));
         path.setStyle(String.format("-fx-border-color: %s;", Settings.getColor()));
+
         icon = getComboBox(iconText);
         icon.setStyle(String.format("-fx-border-color: %s; -fx-border-radius: 10px", Settings.getColor()));
 
-        this.getChildren().addAll(getTitleLabel(name),
+        this.getChildren().addAll(
+                getTitleLabel(name),
                 getInfoLabel(Translator.translate("window.interior.menu.title")),
                 title,
                 getInfoLabel(Translator.translate("window.interior.menu.path")),
                 path,
                 getInfoLabel(Translator.translate("window.interior.menu.icon")),
                 new HBox(icon),
-                getBottomPanel(new HBoxSpacer(), getCancelButton(), confirmButton));
+                getBottomPanel(new HBoxSpacer(), getCancelButton(), confirmButton)
+        );
     }
 
-    protected ComboBox getComboBox(String icon) {
+    /**
+     * Creates and configures a ComboBox for selecting a FontAwesome icon.
+     * The list is styled with graphical previews using {@link FontIcon}.
+     *
+     * @param icon the name of the icon to pre-select
+     * @return a styled {@link ComboBox} populated with FontAwesome icons
+     */
+    protected ComboBox<FontAwesome> getComboBox(String icon) {
         ObservableList<FontAwesome> icons = FXCollections.observableArrayList(FontAwesome.values());
         ComboBox<FontAwesome> comboBox = getComboBox(icons);
 
@@ -86,6 +124,7 @@ class MenuItemWindow extends InteriorWindow {
                 }
             }
         });
+
         return comboBox;
     }
 }
